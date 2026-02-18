bf6540f01332237702c975e18bd1f7065c4b0948
ColinSuther <sutherlandcolin781@gmail.com>
2026-02-18 11:45:53 +0200

    Second commit - Still nothing there.

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -4,8 +4,15 @@ public class JavaApplication2
 {
 
     public static void main(String[] args) 
-    {
-
+    {   
+        /*
+            What is a string?
+            String is an array of charcters.
+            For example: 
+            A string with the word "john" is an array that looks like [j][o][h][n]
+            [x0001][x0002]...etc.
+        */
+        System.out.println("My name is John");
     }
     
 }
