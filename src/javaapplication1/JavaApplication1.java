0bb1c86491c5acea57cbf8c816d05f58fd73015d
ColinSuther <sutherlandcolin781@gmail.com>
2026-02-19 11:15:10 +0200

    Third commit - Some coding practice about input and string manipulation.

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,30 @@
+package javaapplication1;
+
+/*
+    We are in the file viewer, and we are viewing the JavaApplication1 class 
+    that belongs in javaapplication1 package.
+    public = access modifier; gives access to other files
+    public, private, protected 
+
+    block{ }
+*/
+
+public class JavaApplication1 
+{
+    /*
+        static is one copy of something   
+        Data types:
+        int (0 - infinity), 
+        double (exp. 1414.1414) (2mb), 
+        float (exp. 123.736) (600 bytes), 
+        String, 
+    */
+
+    public static void main(String[] args) 
+    {   
+        //variables
+        float myAge = 23;
+        float myID = 11;
+    }
+    
+}
